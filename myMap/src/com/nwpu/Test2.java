64b07b33b1e9ece467f31b2aa9c76167e1674a21
oupuxi <ljtao@mail.nwpu.edu.cn>
2024-04-15 20:25:04 +0800

    集合练习

---
+++ myMap/src/com/nwpu/Test2.java
@@ -0,0 +1,41 @@
+package com.nwpu;
+
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Test2 {
+    public static void main(String[] args) {
+        /* 班级里有N个学生
+        要求：
+        70%的概率随机到男生
+        30%的概率随机到女生
+
+        "范闲","范建","范统","杜子腾","宋合泛","侯笼藤","朱益群","朱穆朗玛峰",
+        "杜琦燕","袁明媛","李猜","田蜜蜜",
+        */
+
+        ArrayList<String> boyList = new ArrayList<>();
+        ArrayList<String> girlList = new ArrayList<>();
+        Collections.addAll(boyList,"范闲","范建","范统","杜子腾","宋合泛","侯笼藤","朱益群","朱穆朗玛峰");
+        Collections.addAll(girlList,"杜琦燕","袁明媛","李猜","田蜜蜜");
+
+        ArrayList<Integer> list = new ArrayList<>();
+        Collections.addAll(list,1,1,1,1,1,1,1);
+        Collections.addAll(list,0,0,0);
+        Collections.shuffle(list);
+        Random r = new Random();
+        int index = r.nextInt(list.size());
+        int number = list.get(index);
+        System.out.println(number);
+        if (number==1){
+            String name = boyList.get(r.nextInt(boyList.size()));
+            System.out.println(name);
+        }else {
+            String name = girlList.get(r.nextInt(girlList.size()));
+            System.out.println(name);
+        }
+
+    }
+}
