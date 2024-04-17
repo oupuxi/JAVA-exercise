c1e63a5c64a68508ef6e6914e5eb61392fb1d103
Laptop-computer <897135767@qq.com>
2024-04-17 17:12:52 +0800

    斗地主

--- myMap/src/com/nwpu/doudizhu1/PokerGame.java
+++ myMap/src/com/nwpu/doudizhu1/PokerGame.java
@@ -21,15 +21,6 @@ public class PokerGame {
         String[] color = {"♦", "♣", "♥", "♠"};
         String[] number = {"3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2"};
 
-        /*for (String c : color) {
-            //c依次表示每一种花色
-            for (String n : number) {
-                //n 依次表示每一个数字
-                list.add(c + n);
-            }
-        }
-        list.add("小王");
-        list.add("大王");*/
         //编号
         int serialNumber = 1;
         for (String n : number) {
